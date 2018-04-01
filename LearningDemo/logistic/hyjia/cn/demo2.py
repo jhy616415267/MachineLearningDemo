c2d5d71273366d32b79ecda43ecf95d9c0981665
jiahongying <jiahongying@baidu.com>
2018-04-01 21:11:31 +0800

    数据模拟一般梯度上升算法

--- LearningDemo/logistic/hyjia/cn/demo2.py
+++ LearningDemo/logistic/hyjia/cn/demo2.py
@@ -1,1 +1,178 @@
 # _*_coding:UTF-8 _*_
+
+import matplotlib.pyplot as plt
+import  numpy as np
+
+
+def loadDataSet():
+    dataMat = []
+    lableMat = []
+    fr = open("testSet.txt")
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0,lineArr[0],float(lineArr[1])])
+        lableMat.append(int(lineArr[2]))
+    fr.close()
+    return dataMat,lableMat
+
+
+def plotDataSet():
+    dataMat,lableMat = loadDataSet()
+    dataArr = np.array(dataMat)
+    n= np.shape(dataMat)[0]
+    xcord1 = [];ycord1 = []
+    xcord2= [];ycord2 = []
+
+    for i in range(n):
+        if int(lableMat[i]) == 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)                                            #添加subplot
+    ax.scatter(xcord1, ycord1, s = 20, c = 'red', marker = 's',alpha=.5)#绘制正样本
+    ax.scatter(xcord2, ycord2, s = 20, c = 'green',alpha=.5)            #绘制负样本
+    plt.title('DataSet')                                                #绘制title
+    plt.xlabel('x'); plt.ylabel('y')                                    #绘制label
+    plt.show()
+
+
+
+69
+# -*- coding:UTF-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+函数说明:加载数据
+ 
+Parameters:
+    无
+Returns:
+    dataMat - 数据列表
+    labelMat - 标签列表
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Zhihu:
+    https://www.zhihu.com/people/Jack--Cui/
+Modify:
+    2017-08-28
+"""
+def loadDataSet():
+    dataMat = []                                                        #创建数据列表
+    labelMat = []                                                        #创建标签列表
+    fr = open('testSet.txt')                                            #打开文件
+    for line in fr.readlines():                                            #逐行读取
+        lineArr = line.strip().split()                                    #去回车，放入列表
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])        #添加数据
+        labelMat.append(int(lineArr[2]))                                #添加标签
+    fr.close()                                                            #关闭文件
+    return dataMat, labelMat                                            #返回
+
+"""
+函数说明:绘制数据集
+ 
+Parameters:
+    无
+Returns:
+    无
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Zhihu:
+    https://www.zhihu.com/people/Jack--Cui/
+Modify:
+    2017-08-28
+"""
+def plotDataSet():
+    dataMat, labelMat = loadDataSet()                                    #加载数据集
+    dataArr = np.array(dataMat)                                            #转换成numpy的array数组
+    n = np.shape(dataMat)[0]                                            #数据个数
+    xcord1 = []; ycord1 = []                                            #正样本
+    xcord2 = []; ycord2 = []                                            #负样本
+    for i in range(n):                                                    #根据数据集标签进行分类
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])    #1为正样本
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])    #0为负样本
+    fig = plt.figure()
+    ax = fig.add_subplot(111)                                            #添加subplot
+    ax.scatter(xcord1, ycord1, s = 20, c = 'red', marker = 's',alpha=.5)#绘制正样本
+    ax.scatter(xcord2, ycord2, s = 20, c = 'green',alpha=.5)            #绘制负样本
+    plt.title('DataSet')                                                #绘制title
+    plt.xlabel('x'); plt.ylabel('y')                                    #绘制label
+    plt.show()                                                            #显示
+
+
+def sigimoid(intX):
+    return 1.0/(1+np.exp(-intX))
+
+
+def gradAscent(dataMatIn,classLabels):
+    dataMatrix = np.mat(dataMatIn)
+    lableMat = np.mat(classLabels).transpose()
+    m,n = np.shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = np.ones((n,1))
+
+    for k in range(maxCycles):
+        h = sigimoid(dataMatrix*weights)
+        error = lableMat - h
+        weights =weights +alpha*dataMatrix.transpose()*error
+    return weights.getA()
+
+
+
+def plotBestFit(weights):
+    dataMat, labelMat = loadDataSet()                                    #加载数据集
+    dataArr = np.array(dataMat)                                            #转换成numpy的array数组
+    n = np.shape(dataMat)[0]                                            #数据个数
+    xcord1 = []; ycord1 = []                                            #正样本
+    xcord2 = []; ycord2 = []                                            #负样本
+    for i in range(n):                                                    #根据数据集标签进行分类
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])    #1为正样本
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])    #0为负样本
+    fig = plt.figure()
+    ax = fig.add_subplot(111)                                            #添加subplot
+    ax.scatter(xcord1, ycord1, s = 20, c = 'red', marker = 's',alpha=.5)#绘制正样本
+    ax.scatter(xcord2, ycord2, s = 20, c = 'green',alpha=.5)            #绘制负样本
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+    plt.title('BestFit')                                                #绘制title
+    plt.xlabel('X1'); plt.ylabel('X2')                                    #绘制label
+    plt.show()
+
+
+
+
+
+if __name__ == '__main__':
+    dataMat, labelMat = loadDataSet()
+    weights = gradAscent(dataMat, labelMat)
+    plotBestFit(weights)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
