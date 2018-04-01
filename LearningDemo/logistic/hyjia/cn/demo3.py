237f37046a08e2fe73bb587326a1ce4003b32a74
jiahongying <jiahongying@baidu.com>
2018-04-01 22:18:19 +0800

    随机梯度上升算法

--- LearningDemo/logistic/hyjia/cn/demo3.py
+++ LearningDemo/logistic/hyjia/cn/demo3.py
@@ -1,7 +1,8 @@
-# _*_coding:UTF-8 _*_
-
+# -*- coding:UTF-8 -*-
+from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 """
 函数说明:加载数据
@@ -31,7 +32,6 @@ def loadDataSet():
     fr.close()                                                            #关闭文件
     return dataMat, labelMat                                            #返回
 
-
 """
 函数说明:sigmoid函数
  
@@ -50,36 +50,6 @@ Modify:
 """
 def sigmoid(inX):
     return 1.0 / (1 + np.exp(-inX))
-
-"""
-函数说明:梯度上升算法
- 
-Parameters:
-    dataMatIn - 数据集
-    classLabels - 数据标签
-Returns:
-    weights.getA() - 求得的权重数组(最优参数)
-Author:
-    Jack Cui
-Blog:
-    http://blog.csdn.net/c406495762
-Zhihu:
-    https://www.zhihu.com/people/Jack--Cui/
-Modify:
-    2017-08-28
-"""
-def gradAscent(dataMatIn, classLabels):
-    dataMatrix = np.mat(dataMatIn)                                        #转换成numpy的mat
-    labelMat = np.mat(classLabels).transpose()                            #转换成numpy的mat,并进行转置
-    m, n = np.shape(dataMatrix)                                            #返回dataMatrix的大小。m为行数,n为列数。
-    alpha = 0.001                                                        #移动步长,也就是学习速率,控制更新的幅度。
-    maxCycles = 500                                                        #最大迭代次数
-    weights = np.ones((n,1))
-    for k in range(maxCycles):
-        h = sigmoid(dataMatrix * weights)                                #梯度上升矢量化公式
-        error = labelMat - h
-        weights = weights + alpha * dataMatrix.transpose() * error
-    return weights.getA()                                                #将矩阵转换为数组，返回权重数组
 
 """
 函数说明:绘制数据集
@@ -119,7 +89,39 @@ def plotBestFit(weights):
     plt.xlabel('X1'); plt.ylabel('X2')                                    #绘制label
     plt.show()
 
+"""
+函数说明:改进的随机梯度上升算法
+ 
+Parameters:
+    dataMatrix - 数据数组
+    classLabels - 数据标签
+    numIter - 迭代次数
+Returns:
+    weights - 求得的回归系数数组(最优参数)
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Zhihu:
+    https://www.zhihu.com/people/Jack--Cui/
+Modify:
+    2017-08-31
+"""
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m,n = np.shape(dataMatrix)                                                #返回dataMatrix的大小。m为行数,n为列数。
+    weights = np.ones(n)                                                       #参数初始化
+    for j in range(numIter):
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4/(1.0+j+i)+0.01                  # _*_coding:UTF-8 _*_                           #降低alpha的大小，每次减小1/(j+i)。
+            randIndex = int(random.uniform(0,len(dataIndex)))                #随机选取样本
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))                    #选择随机选取的一个样本，计算h
+            error = classLabels[randIndex] - h                                 #计算误差
+            weights = weights + alpha * error * dataMatrix[randIndex]       #更新回归系数
+            del(dataIndex[randIndex])                                         #删除已经使用的样本
+    return weights                                                            #返回
+
 if __name__ == '__main__':
     dataMat, labelMat = loadDataSet()
-    weights = gradAscent(dataMat, labelMat)
+    weights = stocGradAscent1(np.array(dataMat), labelMat)
     plotBestFit(weights)
